15fad3543e017f1f4af81081870108e5e9b60d19
Chrisl154 <christian.massie@gmail.com>
2017-02-20 16:06:22 -0700

    FIRST PUB

---
+++ ex1.rb
@@ -0,0 +1,109 @@
+# This denotes Ex0. Basic puts command.
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather you 'non'."
+# puts 'I "said" do not touch this.'
+
+
+# This line denotes Ex2. Explaination of comments.
+# Using the pound sign allows you to 'Disable' or remove from main line a
+#line of code that you do not want ran,
+# or testing what happens with out it.
+
+
+
+# Ex3 - Math
+# Denotes Plus = '+'
+# Minus = '-'
+# Slash = '/'
+# Asterisk = '*'
+# Percent = '%'
+# Less Than '<'
+# Greater than '>'
+# Less than-equal '<='
+# Greater than-equal '>='
+
+# puts "I will now count my Chickens:"
+
+# puts "Hens #{25 + 30 / 6}"
+# puts "Roosters #{100 - 25 * 3 % 4}"
+
+# puts "Now I will count the eggs:"
+
+# puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# puts "Is it true that 3 + 2 < 5 - 7?"
+
+# puts 3 + 2 < 5 - 7
+
+# puts "What is 3 + 2? #{3 + 2}"
+# puts "What is 5 - 7? #{5 - 7}"
+
+# puts "Oh, that's why it's false."
+
+# puts "How about some more."
+
+# puts "Is is greater? #{5 > -2}"
+# puts "Is it greater or equal? #{5 >= -2}"
+# puts "Is it less or equal? #{5 <= -2}"
+#End of Ex3
+
+#Beginning of Ex4 - Variables and Names
+#Equals acts as a definition for it to pull from. Typical variable names in Ruby
+#consist of I, X, J. You are not limited to these however.
+#the use of 4.0 is a floating point number. It could be used as 4, but its better to use
+#4.0 as the number to make it floating point.
+#use of _ allows for longer names and easier identification.
+
+# cars = 100
+# space_in_a_car = 4
+# drivers = 30
+# passengers = 90
+# cars_not_driven = cars - drivers
+# cars_driven = drivers
+# carpool_capacity = cars_driven * space_in_a_car
+# average_passengers_per_car = passengers / cars_driven
+
+# puts "There are #{cars} cars available."
+# puts "There are only #{drivers} drivers available."
+# puts "There will be #{cars_not_driven} empty cars today."
+# puts "We can transport #{carpool_capacity} people today."
+# puts "We have #{passengers} to carpool today."
+# puts "We need to put about #{average_passengers_per_car} people in each car."
+
+#End of ex4.
+
+#Beginning of Ex5 - More Variables and Printing
+# " (Double Quotes) makes a string around a set of text. A string is how you make something
+#that your program might give to a human. You print strings, save strings to files
+#send strings to web servers, and many other things.
+#strings are really handy, so in this exercise you will learn how to make strings that have variables
+#embedded in them. You embed variables inside a string by using a special #{} sequence
+#and then put the variable you want inside the {} characters. This tells Ruby, "Hey, this string
+#needs to be formatted. Put these variables in there."
+
+# my_name = 'Christian Massie'
+# my_age = 25.0 # written in 2017 ;)
+# my_height = 66.0 # inches
+# my_weight = 138.0 # lbs
+# my_eyes = 'Blue'
+# my_teeth = 'White'
+# my_hair = 'Black'
+
+
+
+# puts "Lets talk about #{my_name}"
+# puts "He's #{my_height} inches tall."
+# puts "He's #{my_weight} pounds heavy."
+# puts "Actually...Thats really not heavy."
+# puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+# puts "His teeth are usually #{my_teeth} depending on the soda."
+
+# puts "If I add my age #{my_age}, my height #{my_height}, and my weight #{my_weight} I get #{my_age + my_height + my_weight}."
+
+# End of ex5
+
+# Beginning of ex6 - Strings and Text
