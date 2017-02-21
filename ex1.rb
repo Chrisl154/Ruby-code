95fbb475ca22553146b01840cfe2585e54aba600
Chrisl154 <christian.massie@gmail.com>
2017-02-21 11:24:36 -0700

    Begin of ex13

--- ex1.rb
+++ ex1.rb
@@ -262,13 +262,43 @@
 
 # Beginning of Ex11 - Asking Questions
 
-  print "How old are you? "
-  age = gets.chomp
-  print "How tall are you? "
-  height = gets.chomp
-  print "How much do you weigh? "
-  weight = gets.chomp
+#  print "How old are you? "
+#  age = gets.chomp
+#  print "How tall are you? "
+#  height = gets.chomp
+#  print "How much do you weigh? "
+#  weight = gets.chomp
 
-  puts "So, you're #{age} old, #{height} tall and weigh #{weight}."
+#  puts "So, you're #{age} old, #{height} tall and weigh #{weight}."
 
 #  gets.chomp = It pulls the value for the listed variable from user input. Aka Line 265 - 266.
+
+# Beginning of Ex12
+
+  # print "Give me a number: "
+  # number = gets.chomp.to_i
+
+  # bigger = number * 100
+  # puts "A bigger number is #{bigger}."
+
+  # print "Give me another number: "
+  # another = gets.chomp
+  # number = another.to_f
+
+  # smaller = number / 100
+  # puts "A smaller number is #{smaller}."
+
+# Notes
+# to_i - makes the recieved input as a integer. number = gets.chomp.to_i - This puts whatever number (inputted number) as a integer.
+# to_f - This makes the number inputted a fraction.
+# Test program Ex12
+  #  print "I need money, how much can you give me: "
+  #  money = gets.chomp.to_i
+
+  #  returned_money = money * 0.1
+  #  puts "Thank you! I can give you back this much #{returned_money}."
+
+  #  puts "Now I can afford my eletric bill!"
+# End of Ex12
+
+# Beginning of Ex13 - Parameters, Unpacking, Variables
