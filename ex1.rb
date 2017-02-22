b7f0086b76041ebbcd935625489e66466af94247
Chrisl154 <christian.massie@gmail.com>
2017-02-21 20:58:19 -0700

    End of ex13

--- ex1.rb
+++ ex1.rb
@@ -304,19 +304,23 @@
 # Beginning of Ex13 - Parameters, Unpacking, Variables
 
 
-first, second, third = ARGV
+  #first, second, third = ARGV
 
-  puts "Your first variable is: #{first}"
-  puts "Your second variable is: #{second}"
-  puts "Your third variable is: #{third}"
+  #puts "Your first variable is: #{first}"
+  #puts "Your second variable is: #{second}"
+  #puts "Your third variable is: #{third}"
 
-  print "Whats your favorite color? "
-  color = $stdin.gets.chomp
+  #print "Whats your favorite color? "
+  #color = $stdin.gets.chomp
 
-  print "So your favorite color is #{color}. Right? "
-  right = $stdin.gets.chomp
-  if right == yes
-    then puts "Great!"
-  else
-    then puts "Whoops, We had a problem there. Re-Run me!"
-  end
+  #print "So your favorite color is #{color}. Right? "
+  #right = $stdin.gets.chomp
+  #if right == "yes" && "Yes" && "Right" && "Correct" #can not seem to get mulitple "true" cases to work. Will come back with another formula.
+     #puts "Great!"
+  #else
+     #puts "Whoops, We had a problem there. Re-Run me!"
+  #end
+
+  #End of Ex13
+
+  
