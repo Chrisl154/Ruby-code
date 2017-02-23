c654d8f74d7d35c5c748cce527f4babfa0af72c1
Chrisl154 <christian.massie@gmail.com>
2017-02-22 19:26:09 -0700

    Ex15 Start/finish

--- ex1.rb
+++ ex1.rb
@@ -321,6 +321,52 @@
      #puts "Whoops, We had a problem there. Re-Run me!"
   #end
 
-  #End of Ex13
+# End of Ex13
 
-  
+# Beginning of Ex14
+
+# Let's do one exercise that uses "ARGV" and "gets.chomp" together to ask the user something specific.
+# You will need this for the next exercise where we learn to read and write files.
+# In this exercise we'll use "gets.chomp" slightly differently by having it print a simple ">" prompt.
+# This is similar to a game like Zork or Adventure.
+
+  #user_name = ARGV.first  # gets the first argument
+  #prompt = '> '
+
+  #puts "Hi #{user_name}."
+  #puts "I'd like to ask you a few questions."
+  #puts "Do you like me #{user_name}? "
+  #puts prompt
+  #likes = $stdin.gets.chomp
+
+  #puts "Where do you live #{user_name}? "
+  #puts prompt
+  #lives = $stdin.gets.chomp
+
+# a comma for puts is like using it twice
+  #puts "What kind of computer do you have? ", prompt
+  #computer = $stdin.gets.chomp
+
+  #puts """
+  #Alright, so you said #{likes} about liking me.
+  #You live in #{lives}. Not sure where that is.
+  #And you have a #{computer} computer. Awesome!
+  #"""
+
+# End of Ex14
+
+# Beginning Ex15
+
+  filename = ARGV.first
+
+  txt = open(filename)
+
+  puts "Here's your file #{filename}:"
+  print txt.read
+
+  print "Type the filename again: "
+  file_again = $stdin.gets.chomp
+
+  txt_again = open(file_again)
+
+  print txt_again.read
