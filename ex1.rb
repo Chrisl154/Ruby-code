6cea004249fb1dc1df14bf099d60357be5676b41
Chrisl154 <christian.massie@gmail.com>
2017-03-09 20:20:26 -0700

    Ex18 Full

--- ex1.rb
+++ ex1.rb
@@ -554,3 +554,26 @@ print_none()
 # unpacking arguments and just use the names we want right inside the (). Thats what print_two_again does.
 # After that you have an example of how you make a function that takes one argument in print_one.
 # Finally you have a function that has no arguments in print_none.
+
+
+# Check list for functions :
+# 1: Did you start your function definition with 'def'?
+# 2: Does your function name have only characters and '_'(underscore) characters?
+# 3: Did you put an open parenthesis '(' right after the function name?
+# 4: Did you put your arguments after the parenthesis '(' seperated by commas?
+# 5: Did you make each argument uniquie (meaning no duplicated names)?
+# 6: Did you put a close parenthesis ')' after the arguments?
+# 7: Did you indent all lines of code you want in the function two spaces?
+# 8: Did you end your function with 'end' lined up with the 'def' above?
+
+# When you run ("use" or "call") a function, check these things:
+# 1: Did you call/use/run this function by typing its name?
+# 2: Did you put the '(' character after the name to run it?
+# 3: Did you put the vaules you want into the parenthesis separated by commas?
+# 4: Did you end the function call with a ')' character?
+# 5: Functions that dont have parameters do not need the () after them, but would it be clearer if you wrote them anyway?
+
+# Use these two checlists on the ramaining lessons until you do not need them anymore.
+# Finally, repeat this a few times to yourself:
+
+# "To 'run,' 'call,' or 'use' a function all mean the same thing."
