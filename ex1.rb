1438f36b6f9acf105ea8c69e5b4ca243f41db415
Chrisl154 <christian.massie@gmail.com>
2017-03-12 16:24:56 -0600

    Beginning ex19

--- ex1.rb
+++ ex1.rb
@@ -515,30 +515,30 @@
 
 # this one is like your scipts with ARGV
 
-def print_two(*args)
-  arg1, arg2 = args
-  puts "arg1: #{arg1}, arg2: #{arg2}"
-end
+#def print_two(*args)
+#  arg1, arg2 = args
+#  puts "arg1: #{arg1}, arg2: #{arg2}"
+#end
 
 # ok, that *args is actually pointless, we can judt do this
-def print_two_again(arg1, arg2)
-  puts "arg1: #{arg1}, arg2: #{arg2}"
-end
+#def print_two_again(arg1, arg2)
+#  puts "arg1: #{arg1}, arg2: #{arg2}"
+#end
 
 # this just takes one argument
-def print_one(arg1)
-  puts "arg1: #{arg1}"
-end
+#def print_one(arg1)
+#  puts "arg1: #{arg1}"
+#end
 
 # this one takes no arguments
-def print_none()
-  puts "I got nothin'."
-end
+#def print_none()
+#  puts "I got nothin'."
+#end
 
-print_two("Christian","Massie")
-print_two_again("Christian", "Massie")
-print_one("First!")
-print_none()
+#print_two("Christian","Massie")
+#print_two_again("Christian", "Massie")
+#print_one("First!")
+#print_none()
 
 # End of Coding section
 # 1: First we tell ruby we want to make a function using 'def' for "define"
@@ -578,3 +578,28 @@ print_none()
 
 # "To 'run,' 'call,' or 'use' a function all mean the same thing."
 # A function can start with anything not a number, and can contain letters, numers, and underscores.
+
+#End of Ex18
+
+#Beginning Of Ex19
+#def cheese_and_crackers (cheese_count, boxes_of_crackers)
+#  puts "You have #{cheese_count} slices of cheese!"
+#  puts "You have #{boxes_of_crackers} boxes of crackers!"
+#  puts "Man that's enouugh for a party!"
+#  puts "Get a blanket. \n"
+#end
+
+#puts "We can just give the functions numers directly:"
+#cheese_and_crackers(20, 30)
+
+#puts "OR, we can use variables from our script:"
+#amount_of_cheese = 10
+#amount_of_crackers = 50
+
+#cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+#puts "We can even do math inside too:"
+#cheese_and_crackers(10 + 20, 5 + 6)
+
+#puts "And we can combine the two, variables and math:"
+#cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
