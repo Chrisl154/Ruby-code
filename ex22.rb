d1a6a2471969b48dcc71fba72495b293546222f2
Chrisl154 <christian.massie@gmail.com>
2017-03-22 17:29:01 -0600

    Added lines. Still more to go.

--- ex22.rb
+++ ex22.rb
@@ -1,1 +1,14 @@
 # Beginning of Ex22
+
+# - This allows the coder to disable or render a line useless, also used for comments in the line.
+# puts - This shows a comment or some sort of line for sentences that are within quotations.
+# # Denotes Plus = '+'
+# Minus = '-'
+# Slash = '/'
+# Asterisk = '*'
+# Percent = '%'
+# Less Than '<'
+# Greater than '>'
+# Less than-equal '<='
+# Greater than-equal '>='
+# #{} - Variable that is used in a puts or print command.
