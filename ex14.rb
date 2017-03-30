66280273ac03656fbd5f9a2cb492d37337469b55
Chrisl154 <christian.massie@gmail.com>
2017-03-30 14:50:44 -0600

    Change of ex14 add of ex23

--- ex14.rb
+++ ex14.rb
@@ -5,27 +5,27 @@
 # In this exercise we'll use "gets.chomp" slightly differently by having it print a simple ">" prompt.
 # This is similar to a game like Zork or Adventure.
 
-  #user_name = ARGV.first  # gets the first argument
-  #prompt = '> '
+  user_name = ARGV.first  # gets the first argument
+  prompt = '> '
 
-  #puts "Hi #{user_name}."
-  #puts "I'd like to ask you a few questions."
-  #puts "Do you like me #{user_name}? "
-  #puts prompt
-  #likes = $stdin.gets.chomp
+  puts "Hi #{user_name}."
+  puts "I'd like to ask you a few questions."
+  puts "Do you like me #{user_name}? "
+  puts prompt
+  likes = $stdin.gets.chomp
 
-  #puts "Where do you live #{user_name}? "
-  #puts prompt
-  #lives = $stdin.gets.chomp
+  puts "Where do you live #{user_name}? "
+  puts prompt
+  lives = $stdin.gets.chomp
 
 # a comma for puts is like using it twice
-  #puts "What kind of computer do you have? ", prompt
-  #computer = $stdin.gets.chomp
+  puts "What kind of computer do you have? ", prompt
+  computer = $stdin.gets.chomp
 
-  #puts """
-  #Alright, so you said #{likes} about liking me.
-  #You live in #{lives}. Not sure where that is.
-  #And you have a #{computer} computer. Awesome!
-  #"""
+  puts """
+  Alright, so you said #{likes} about liking me.
+  You live in #{lives}. Not sure where that is.
+  And you have a #{computer} computer. Awesome!
+  """
 
 # End of Ex14
